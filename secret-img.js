4031155f25f8aaf9d01b7541ae4beba531fbd6b4
Arthur Fernandez <arthur@snooget.com>
2023-04-25 16:41:06 -0700

    added changes and button now opens

--- secret-img.js
+++ secret-img.js
@@ -1,15 +1,30 @@
+const buttonElement = document.getElementById('secret-button');
+const imgElement = document.getElementById('secret-img');
+ 
+const toggleHiddenElement = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+ 
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(imgElement);
+});
 // below is the JS for the toggle hidden img
-const imgElement = document.getElementById('secret-img-btn');
+/*const imgElement = document.getElementById('secret-img-btn');
 const imgBtn = document.getElementById('secret-img');
 
-const toggleHiddenElementImg = (domElementImg) => {
-    if (domElementImg.style.display === 'none') {
-      domElementImg.style.display === 'none';
+const toggleHiddenElementImg = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display === 'block';
   } else {
-    domElementImg.style.display = 'none';
+    domElement.style.display = 'none';
   }
 }
 
 imgElement.addEventListener('click', () => {
   toggleHiddenElementImg(imgBtn);
 });
+*/
