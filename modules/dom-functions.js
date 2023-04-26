fb5431b6d653c258adf754e3076a68beaf990a89
Arthur Fernandez <arthur@snooget.com>
2023-04-25 18:32:01 -0700

    corrected dom-function === to =

--- modules/dom-functions.js
+++ modules/dom-functions.js
@@ -2,7 +2,7 @@
 
 const toggleHiddenElement = (domElement) => {
     if (domElement.style.display === 'none') {
-        domElement.style.display === 'block';
+        domElement.style.display = 'block';
     } else {
         domElement.style.display = 'none';
     }
