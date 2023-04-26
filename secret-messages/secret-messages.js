6ba981d6dc82c2e68415457c58a368ea10da61c6
Arthur Fernandez <arthur@snooget.com>
2023-04-25 18:00:34 -0700

    added html

--- secret-messages/secret-messages.js
+++ secret-messages/secret-messages.js
@@ -1,2 +1,21 @@
 /* secret-messages.js */
-import { toggleHiddenElement, changeToFunkyColor } from '../modules/dom-functions';
+import { toggleHiddenElement, changeToFunkyColor } from '../modules/dom-functions.js';
+
+const buttonElement = document.getElementById('secret-button');
+const pElement =  document.getElementById('secret-p');
+// Above are two delared variables assigned to html elements...
+
+buttonElement.addEventListener('click', () => {
+    toggleHiddenElement(pElement);
+    changeToFunkyColor(buttonElement);
+});
+
+/*
+Changes broken down:
+
+    - secret-messages.js has two functions toggleHiddenElement and changeToFunkyColor() which are imported from the ../modules/dom-functions.js
+
+    - This allows the imported toggleHiddenElement() function to execute with pElement as an argument when the button is clicked...
+
+    - changeToFunkyColor() is another imported function which changes with the click called with buttonElement.
+*/
