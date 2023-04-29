e2f314517789a39411f4063c72a80ba8d26a66ad
Arthur Fernandez <arthur@snooget.com>
2023-04-28 18:45:07 -0700

    added two example for loops

--- random.js
+++ random.js
@@ -17,7 +17,7 @@ const numbers = [5, 3, 9, 30];
 const smallestPowerOfTwo = (arr) => {
   let results = [];
   // The 'outer' for loop - loops through each element in the array
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     number = arr[i];
     // The 'inner' while loop - searches for smallest power of 2 greater than the given number
   i = numbers;
